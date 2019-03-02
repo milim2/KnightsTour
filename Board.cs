65999c6d7056b453893bb96d7b9fdc8ac7ccf729
milimlee <milimlee0@gmail.com>
2019-03-01 23:20:48 -0500

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsTourEx2
+{
+    public class Board 
+    {
+        public int[,] board;
+        public const int DIMENSION = 8;
+        
+        void ChessBoard()
+        {
+            board = new int[DIMENSION, DIMENSION];      
+        }
+    }
+}
